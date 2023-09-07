7c6037c500c4a1c6f6696cef14abc6536f39e69f
Harry Whyte~ <harrywhyte@kubrickgroup.com>
2023-09-07 16:17:51 +0100

    Added some express functionality to myApp.js - includes get and use method to deploy static assets

--- myApp.js
+++ myApp.js
@@ -1,9 +1,19 @@
 let express = require('express');
 let app = express();
 
+console.log("Hello World"); 
 
+// absolutePath = __dirname + '/views/index.html'
 
+// app.get('/', function(req,res) {
+//   res.send('Hello Express')
+// })
 
+app.get('/', function(req,res) {
+  res.sendFile (__dirname + '/views/index.html')
+})
+
+app.use('/public', (express.static(__dirname + '/public')))
 
 
 
